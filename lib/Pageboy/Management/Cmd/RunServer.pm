a4dbb92931211ea7ff26d96c4ebf9b7f52cf5e77
Hakim Cassimally <hakim@doesliverpool.com>
2015-09-28 00:26:41 +0000

    bin/manage runserver

---
+++ lib/Pageboy/Management/Cmd/RunServer.pm
@@ -0,0 +1,45 @@
+package Pageboy::Management::Cmd::RunServer;
+
+use Moo;
+use MooX::Cmd;
+use MooX::Options protect_argv => 0;
+
+use feature 'say';
+use Plack::Runner;
+
+has runner => (
+    is => 'lazy',
+    default => sub {
+        my $self = shift;
+        my $runner = Plack::Runner->new;
+        $runner->parse_options(@{ $self->plackup_args });
+        $runner;
+    },
+);
+
+has plackup_args => (
+    is => 'lazy',
+    default => sub {
+        my $self = shift;
+        my @args = @{ $self->command_args };
+        push @args, '-Ilib';
+        push @args, '-r';
+        push @args, '-a' => $self->psgi;
+        \@args;
+    }
+);
+
+has psgi => (
+    is => 'ro',
+    default => 'pageboy.psgi',
+);
+
+sub execute {
+    my ( $self, $args ) = @_;
+
+    # system 'sass', '--update' => 'public/stylesheets';
+
+    $self->runner->run;
+}
+
+1;
