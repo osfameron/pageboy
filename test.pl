605641ec9acf4547b1ff69178840e3d59eaed9db
osfameron <hakim.cassimally@gmail.com>
2015-10-24 13:51:44 +0000

    Removed mistakenly committed file

--- test.pl
+++
@@ -1,7 +0,0 @@
-use strict; use warnings;
-use Geo::IP;
-use Data::Dumper;
-my $gi = Geo::IP->new(GEOIP_MEMORY_CACHE);
-# my $record = $gi->record_by_addr('80.68.93.60');
-my $record = $gi->record_by_addr('82.40.19.60');
-print Dumper($record);
