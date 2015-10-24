f6c669b1391fcf3d0b296f0b3d9bc59609dceaec
osfameron <hakim.cassimally@gmail.com>
2015-10-24 14:48:17 +0000

    Mock Geo IP->city in tests

--- t/lib/Pageboy/Test/Geo.pm
+++ t/lib/Pageboy/Test/Geo.pm
@@ -15,8 +15,30 @@ has _ip => (
     writer => 'set_ip',
 );
 
+has lookup => (
+    is => 'ro',
+    traits => ['Hash'],
+    default => sub {
+        +{
+            '130.88.98.239' => 'Manchester', # man.ac.uk
+            '138.253.13.50' => 'Liverpool',  # liv.ac.uk
+            '145.18.12.36'  => 'Amsterdam',  # uva.nl
+        }
+    },
+    handles => {
+        '_get_location_from_ip' => 'get',
+    },
+);
+
 sub get_ip_from_request ($self, $r) {
     return $self->_ip;
+}
+
+sub get_location_from_ip ($self, $r) {
+    my $ip = $self->get_ip_from_request($r)
+        or return;
+
+    return $self->_get_location_from_ip($ip);
 }
 
 1;
